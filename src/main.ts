323c1c9c9aea7a5658cb2c56814fb7c4c42492fc
Maria Papadelli <Maria Papadelli>
2017-01-30 22:48:17 +0000

    Multiple post it functionality

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 //Get hold of the buttons and post-its into a variable
 
-var p1:HTMLElement = document.getElementById('p1');
+
 var redBtn:HTMLElement = document.getElementById('redBtn');
 var greenBtn:HTMLElement = document.getElementById('greenBtn');
 var blueBtn:HTMLElement = document.getElementById('blueBtn');
 var orangeBtn:HTMLElement = document.getElementById('orangeBtn');
-var debug:HTMLElement = document.getElementById('debug');
+var newPostIt:HTMLElement = document.getElementById('newPostIt');
 var postItArea:HTMLElement = document.getElementById('post-it-area');    
 var body:HTMLElement = document.getElementById('body');    
 
-//set postIt state
-var edited:boolean = false;
 
 //post it starting position parameters
 var startX:number;
@@ -20,23 +18,33 @@ var posY:number
 var offsetX:number;
 var offsetY: number;
 
-//create a paragraph element for the post-it
-var msg = document.createElement('p');
-p1.appendChild(msg);
-msg.style.padding = '5px';
 
-//rotate  post-it
-p1.style.transform ='rotate(4deg)';
+//PostIt class
+class PostIt {
+     p:HTMLElement = document.createElement('div');
+     msgTxt:string;
+     constructor (x:number, y:number) {
+         postItArea.appendChild(this.p);
+         this.p.className = 'post-it';
+         this.p.draggable = true;
+         this.p.style.position = 'absolute';
+         this.p.style.transform = 'rotate(4deg)';
+         this.p.style.backgroundColor = 'lightgreen';
+         this.p.style.left = x + 'px';
+         this.p.style.top = y + 'px';
+         this.msgTxt = '';
 
-p1.style.backgroundColor = "lightgreen";
+     }
 
-//defining the 'draggable' properties of the post-it
-p1.draggable = true;
-p1.style.position = 'relative';
+}
 
+
+//create the first object
+
+var p1:PostIt = new PostIt(100,100);
 
 //On double clicking on the postit call onEdit
-p1.addEventListener('dblclick',onEdit);
+postItArea.addEventListener('dblclick',onEdit);
 
 
 //On clicking on the buttons call changeColor
@@ -45,189 +53,121 @@ greenBtn.addEventListener('click', changeColor);
 blueBtn.addEventListener('click', changeColor);
 orangeBtn.addEventListener('click', changeColor);
 
+//On clicking on the New Post It button create a new post-it
+newPostIt.addEventListener('click', function(event) {
+    let postIt = new PostIt(400, 200);
+})
+
+//On double clicking on the post-it area create a new post-it
+postItArea.addEventListener('dblclick', function(event:any)
+{
+    if (event.target.className !='post-it') {
+        var x, y:number;
+        x = event.clientX-200;
+        y = event.clientY-200;
+        let postIt = new PostIt(x,y);
+    }
+});
+
 //define event listeners on 'draggable post-it
-p1.addEventListener('dragstart',onDragStart);
-p1.addEventListener('drag',onDrag);
-p1.addEventListener('dragend',dragStop);
+postItArea.addEventListener('dragstart',onDragStart);
+postItArea.addEventListener('drag',onDrag);
+postItArea.addEventListener('dragend',dragStop);
 
 //define event listeners for 'drop' area (post-it-area)
 postItArea.addEventListener('drop', onDrop);
 postItArea.addEventListener('dragover',dragOver);
 
 
+
+
+//changeColor function definition
 function changeColor(e: Event):void {
     if (e.srcElement.id == 'redBtn') {
-        p1.style.backgroundColor = 'lightcoral';
+        p1.p.style.backgroundColor = 'lightcoral';
     }
     else if (e.srcElement.id == 'orangeBtn') {
-        p1.style.backgroundColor = 'orange';
+        p1.p.style.backgroundColor = 'orange';
     }
     else if (e.srcElement.id == 'blueBtn') {
-        p1.style.backgroundColor = 'lightblue';
+        p1.p.style.backgroundColor = 'lightblue';
     }
     else {
-        p1.style.backgroundColor = 'lightgreen';
+        p1.p.style.backgroundColor = 'lightgreen';
     }
 }
 
+
+//onDragStart function definition
 function onDragStart(event:any):void {
-    console.log(event.target);
-    startX= event.clientX;
-    startY= event.clientY;
-    offsetX = extractNumber(event.target.style.left);
-    offsetY = extractNumber(event.target.style.top);
+    if (event.target.className == 'post-it') {
+        console.log(event.target);
+        startX= event.clientX;
+        startY= event.clientY;
+        offsetX = extractNumber(event.target.style.left);
+        offsetY = extractNumber(event.target.style.top);
+    }
 }
 
 
+//onDrag function definition
 function onDrag(event: any):void {
-    event.preventDefault();
-    console.log(event.target);
-    
-    posX= event.clientX+ offsetX - startX;
-    posY= event.clientY+ offsetY - startY;
+    if (event.target.className == 'post-it') {
+        event.preventDefault();
+        console.log(event.target);
+        
+        posX= event.clientX+ offsetX - startX;
+        posY= event.clientY+ offsetY - startY;
 
-    //animate and highlight post-it
-    var color:string = event.target.style.backgroundColor;
-    event.target.style.transform ='rotate(0deg)';
-        if (color == 'lightblue') {
-        event.target.style.border = '2px solid steelblue'
+        //animate and highlight post-it
+        var color:string = event.target.style.backgroundColor;
+        event.target.style.transform ='rotate(0deg)';
+            if (color == 'lightblue') {
+            event.target.style.border = '2px solid steelblue'
+        }
+        else if (color == 'lightcoral') {
+            event.target.style.border  = '2px solid red';
+        }
+        else if (color =='orange') {
+            event.target.style.border = '2px solid orangered';
+        }
+        else {
+            event.target.style.border = '2px solid limegreen';
+        }
     }
-    else if (color == 'lightcoral') {
-        event.target.style.border  = '2px solid red';
-    }
-    else if (color =='orange') {
-        event.target.style.border = '2px solid orangered';
-    }
-    else {
-        event.target.style.border = '2px solid limegreen';
-    }
-
 }
 
+
+//dragStop function definition
 function dragStop(event:any):void {
-    event.target.style.transform ='rotate(4deg)';
-    event.target.style.border = 'none';
-    event.preventDefault();
-    console.log(event.target);
-       
-    event.target.style.left = posX + 'px';
-    event.target.style.top = posY + 'px';
-
+    if (event.target.className == 'post-it') {
+        event.target.style.transform ='rotate(4deg)';
+        event.target.style.border = 'none';
+        event.preventDefault();
+        console.log(event.target);
+        
+        event.target.style.left = posX + 'px';
+        event.target.style.top = posY + 'px';
+    }
 }
 
+
+//onDrop function definition
 function onDrop(event: any):void {
     event.preventDefault();
     this.style.border = '';
     event.target.style.border = 'none';
 }
 
+
+//dragOver function definition
 function dragOver(e: Event):void {
     e.preventDefault();
     this.style.border = '3px dotted lightgray';
 }
 
 
-
-function onEdit():void {
-    //save the current color of the post-it
-    var color:string = p1.style.backgroundColor;
-
-    //change edited status and rotate post it
-    edited = true;
-    p1.style.transform ='rotate(0deg)';
-
-
-    //make color buttons hidden
-    redBtn.style.visibility = 'hidden';
-    greenBtn.style.visibility = 'hidden';
-    blueBtn.style.visibility = 'hidden';
-    orangeBtn.style.visibility = 'hidden';
-
-    //clear previous post-it text;
-    p1.removeChild(msg);
-    
-    //Create the input text element and apend it to the post it
-    var msgInput = document.createElement('textarea');
-    msgInput.style.fontFamily = 'Segoe UI';
-    msgInput.placeholder = 'Please enter your message here';
-    msgInput.style.borderStyle = 'none';
-    msgInput.style.background = 'none';
-    p1.appendChild(msgInput);
-    msgInput.style.width = '160px';
-    msgInput.style.height = '160px';
-    msgInput.maxLength = 160;
-    msgInput.style.wordWrap= 'break-word';
-
-
-    if (color == 'lightblue') {
-        p1.style.border = '2px solid steelblue'
-    }
-    else if (color == 'lightcoral') {
-        p1.style.border  = '2px solid red';
-    }
-    else if (color =='orange') {
-        p1.style.border = '2px solid orangered';
-    }
-    else {
-        p1.style.border = '2px solid limegreen';
-    }
-
-    msgInput.focus(); 
-    msgInput.style.border = '1px solid blue';
-    
-    msgInput.addEventListener('keydown',keyPressed);
-
-    document.addEventListener('click',onSave);
-
-    function keyPressed(event: any):void {
-        var keyName:string = event.key;
-        console.log(keyName);
-        switch (keyName) {
-            case 'Enter':
-                onSave();
-                break;
-            default:
-                //Statements executed when none of the values match the value of the expression
-            break;
-        }
-
-    }
-
-    function onSave():void {
-
-        var msgInputText:string = msgInput.value;
-        edited = false;
-        if (msgInputText == '') {
-            console.log('Please enter a message')
-        }
-        else {
-            msg.innerHTML = msgInputText;
-            msg.style.margin = 'auto';
-            msg.style.overflow = 'hidden';
-            msg.style.maxWidth = '100%';
-            msg.style.wordWrap= 'break-word';
-        }
-        p1.removeChild(msgInput);
-        p1.appendChild(msg);
-
-        //make color buttons visible
-        redBtn.style.visibility = 'visible';
-        greenBtn.style.visibility = 'visible';
-        blueBtn.style.visibility = 'visible';
-        orangeBtn.style.visibility = 'visible';
-        
-        
-        p1.style.backgroundColor = color;
-        p1.style.border = 'none'
-        msgInput.style.border = 'none'
-        p1.style.transform ='rotate(4deg)';
-        document.removeEventListener('click',onSave);
-        
-    }
-
-
-}
-
+//extractNumber function definition
 function extractNumber(s:string):number
 {
     var n = parseInt(s);
@@ -236,3 +176,116 @@ function extractNumber(s:string):number
 }
 
 
+function onEdit(event: any):void {
+        if (event.target.className == 'post-it') {
+            //save the current color of the post-it
+            var color:string = event.target.style.backgroundColor;
+    
+            //rotate post it
+            event.target.style.transform ='rotate(0deg)';
+
+
+            //make color buttons hidden
+            redBtn.style.visibility = 'hidden';
+            greenBtn.style.visibility = 'hidden';
+            blueBtn.style.visibility = 'hidden';
+            orangeBtn.style.visibility = 'hidden';
+            
+            //remove previous content
+            if (event.target.childElementCount != 0) {
+                event.target.innerHTML = '';
+            }
+            //Create the input text element and apend it to the post it
+            var msgInput = document.createElement('textarea');
+            msgInput.style.fontFamily = 'Segoe UI';
+            msgInput.placeholder = 'Please enter your message here';
+            msgInput.style.borderStyle = 'none';
+            msgInput.style.background = 'none';
+            event.target.appendChild(msgInput);
+            msgInput.style.width = '160px';
+            msgInput.style.height = '160px';
+            msgInput.maxLength = 160;
+            msgInput.style.wordWrap= 'break-word';
+
+
+            if (color == 'lightblue') {
+                event.target.style.border = '2px solid steelblue'
+            }
+            else if (color == 'lightcoral') {
+                event.target.style.border  = '2px solid red';
+            }
+            else if (color =='orange') {
+                event.target.style.border = '2px solid orangered';
+            }
+            else {
+                event.target.style.border = '2px solid limegreen';
+            }
+
+            msgInput.focus(); 
+            msgInput.style.border = '1px solid blue';
+            
+            msgInput.addEventListener('keydown',keyPressed);
+
+            document.addEventListener('click',onSave);
+        }
+
+        function onSave():void {
+            var msg:HTMLElement = document.createElement('p');
+            var msgInputText:string = msgInput.value;
+            
+            if (msgInputText == '') {
+                console.log('Please enter a message')
+                if (event.target.msgTxt != undefined) {
+                    msg.innerHTML = event.target.msgTxt;
+                }
+                else {
+                    msg.innerHTML = msgInputText;
+                }
+                msg.style.margin = 'auto';
+                msg.style.overflow = 'hidden';
+                msg.style.maxWidth = '100%';
+                msg.style.wordWrap= 'break-word';
+                
+            }
+            else {
+                msg.innerHTML = msgInputText;
+                msg.style.margin = 'auto';
+                msg.style.overflow = 'hidden';
+                msg.style.maxWidth = '100%';
+                msg.style.wordWrap= 'break-word';
+                event.target.msgTxt = msgInputText;
+            }
+            console.log(event.target.msgTxt)
+            event.target.removeChild(msgInput);
+            event.target.appendChild(msg);
+
+            //make color buttons visible
+            redBtn.style.visibility = 'visible';
+            greenBtn.style.visibility = 'visible';
+            blueBtn.style.visibility = 'visible';
+            orangeBtn.style.visibility = 'visible';
+            
+            
+            event.target.style.backgroundColor = color;
+            event.target.style.border = 'none'
+            msgInput.style.border = 'none'
+            event.target.style.transform ='rotate(4deg)';
+            document.removeEventListener('click',onSave);
+            
+        }
+
+        function keyPressed(event: any):void {
+            var keyName:string = event.key;
+            console.log(keyName);
+            switch (keyName) {
+                case 'Enter':
+                    onSave();
+                    break;
+                default:
+                    //Statements executed when none of the values match the value of the expression
+                break;
+            }
+
+        }
+
+}
